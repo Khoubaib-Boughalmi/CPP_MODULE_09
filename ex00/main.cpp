097c284c70d8ec4cfad8ae07af8629445e2f562d
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-22 13:41:44 +0100

    more progress in parse

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,9 +1,12 @@
 #include "BitcoinExchange.h"
 
+//!!!!!!!!!! RETURN OF STRTRIM SHOULD BE FREED
 std::string strTrim(char * passedStr) {
-    size_t start;
+    size_t start; 
     size_t end;
 
+    if(!passedStr || !passedStr[0])
+        return "";
     std::string str(passedStr);
     start = str.find_first_not_of(" \t\r"); 
     if(start == std::string::npos)
@@ -12,14 +15,40 @@ std::string strTrim(char * passedStr) {
     return str.substr(start, end - start + 1);
 }
 
-int parseFirstLine(std::string str)
-{
+void parseSingleLine(std::map<std::string, std::string> &uMap, std::string line) {
+    // char *date = NULL;
+    // char *value = NULL;
+    (void)uMap;
+    char *token = std::strtok(const_cast<char *>(line.c_str()), "|");
+    // date = token;
+    if(token)
+        std::cout << token <<" ";
+    // while(token)
+    // {
+        std::string str;
+    if(!strTrim(token).empty())
+    {
+        token = std::strtok(NULL, "|");
+        if(token)
+            std::cout << token << std::endl;
+        else
+            std::cout << "NULL" << std::endl;
+    //     token = std::strtok(NULL, "|");
+    //     std::cout << token << std::endl;
+    // }
+    }
+}
+
+void parseInput(std::string line) {
+    std::map<std::string, std::string>uMap;
+    parseSingleLine(uMap, line);
+}
+
+int parseFirstLine(std::string str) {
     int counter = 0;
     char *token = std::strtok(const_cast<char *>(str.c_str()), "|");
-    while(token)
-    {
-        switch (counter)
-        {
+    while(token) {
+        switch (counter) {
             case 0:
                 if (strcmp(strTrim(token).c_str(), "date")) return (0);
                 break;
@@ -38,33 +67,25 @@ int parseFirstLine(std::string str)
 int main(int argc, char **argv)
 {
     (void)argv;
-    if(argc != 2)
-    {
+    if(argc != 2) {
         std::cout << "Error: could not open file." << std::endl;
         return (1);
     }
-    // std::string str = " hello   | sadsad |123|||a";
-    // char *token = std::strtok(const_cast<char *>(str.c_str()), "|");
-    // while(token)
-    // {
-    //     std::cout << strTrim(token) << std::endl;
-    //     token = std::strtok(NULL, "|");
-    // }
     std::ifstream inputFile("input.txt");
     if(!inputFile.is_open())
         return (1);
     std::string line;
     int firstLine = 1;
-    while (std::getline(inputFile, line))
-    {
-        if(firstLine)
-        {
+    while (std::getline(inputFile, line)) {
+        if(firstLine) {
             firstLine = 0;
-            if(!parseFirstLine(line))
-            {
+            if(!parseFirstLine(line)) {
                 std::cout << "failed first line\n";
                 return (1);
             }
+        }
+        else {
+            parseInput(line);
         }
     }
     
