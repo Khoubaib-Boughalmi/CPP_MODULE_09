395d17e14ca432cfa1ad91204d916d7f620a336f
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-27 00:26:12 +0100

    started ex01

---
+++ ex01/RPN.h
@@ -0,0 +1,10 @@
+#ifndef RPN_H
+#define RPN_H
+
+#include <iostream>
+#include <stack>
+
+// 1 2 * 2 / 2 + 5 * 6 - 1 3 * - 4 5 * * 8 / 
+// ((((((((((1 * 2) / 2) + 2) * 5) - 6) * 1) - 3) * 4) * 5) / 8) == 15
+
+#endif
